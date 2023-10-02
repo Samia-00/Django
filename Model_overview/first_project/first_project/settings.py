b55f08babfa80d23b3f931fde84ed4ae77fcd733
samia-tm <samia@technometrics.net>
2023-10-02 16:10:02 +0600

    model overview

--- Model_overview/first_project/first_project/settings.py
+++ Model_overview/first_project/first_project/settings.py
@@ -16,7 +16,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,  "template")
-STATIC_DIR = os.path.join(BASE_DIR,"static")
+# STATIC_DIR = os.path.join(BASE_DIR,"static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -121,8 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
