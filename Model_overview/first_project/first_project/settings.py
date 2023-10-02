d4244d30a89975a27b49e488021891dccf29eae7
samia-tm <samia@technometrics.net>
2023-10-02 10:52:49 +0600

    adding model view

--- Model_overview/first_project/first_project/settings.py
+++ Model_overview/first_project/first_project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
