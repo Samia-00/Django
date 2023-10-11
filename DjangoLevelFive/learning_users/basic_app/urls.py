d3b2b26a5ba51f5cd3763d109728fa35cd7c88a9
samia-tm <samia@technometrics.net>
2023-10-11 16:55:08 +0600

    registration

---
+++ DjangoLevelFive/learning_users/basic_app/urls.py
@@ -0,0 +1,9 @@
+# basic_app/urls.py
+from django.conf.urls import url
+from basic_app import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    url(r'^register/$', views.register, name='register'),
+]
